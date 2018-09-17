4854c0ea72d375673dd3f6f463abecb0411c4e0f
dpartida <31483393+dpartida@users.noreply.github.com>
2018-09-17 16:19:22 -0500

    tested modifying Renumber in github folder

--- Renumber.m
+++ Renumber.m
@@ -2,7 +2,7 @@ clear,clc
 
 %main script that calls to functions that solve for Reynold's Number Re
 
-Re=(p*v*c)/u; %calculate Reynold's Number
+Re=(p*v*c)/u; %equation to calculate Reynold's Number
 
 
 
