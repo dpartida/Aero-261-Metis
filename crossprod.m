bcc9a7205f80366973cf7ea2666a3732d7cbf776
dpartida <31483393+dpartida@users.noreply.github.com>
2018-09-19 13:09:51 -0500

    Commit xfoil analysis C-5 root airfoil

--- crossprod.m
+++ crossprod.m
@@ -16,7 +16,7 @@ x=det(i); %determinant of i
 y=-det(j); %determinant of j
 z=det(k); %determinant of k
 
-fprintf('The cross product in vector cartesian form is <%4.2fi, %4.2fj, %4.2fk>\n', x, y, z)
+fprintf('The cross product in vector cartesian form is <%4.2fi - %4.2fj + %4.2fk>\n', x, y, z)
 m=sqrt(x^2+y^2+z^2);
 
 al=acosd(x/m); %alpha coordinate direction angle w/ x axis
