1cf2870a7b68efb7b04229384a142b5f82b01bf7
adam orgler <aorgler@iastate.edu>
2018-09-21 16:26:45 -0500

    changed scriptExtract

--- MATLAB Stuff/slope2D_3D.m
+++ MATLAB Stuff/slope2D_3D.m
@@ -8,7 +8,5 @@ c=10; %average chord length
 sw=25; %sweep angle of 25 degrees
 AR=b/c; %equation for aspect ratio
 e=1.78*(1-(0.045*(AR^0.68)))-0.64; %calculates span efficiency factor
-
-a0=0.115 %2D airfoil lift curve slope
-
-a=(a0*cosd(sw))/(1+(1/(pi*e*AR))*a0*cosd(sw)) %equation to convert 2D lift curve slope to 3D lift curve slope
+a0=0.115; %2D airfoil lift curve slope
+a=(a0*cosd(sw))/(1+(1/(pi*e*AR))*a0*cosd(sw)); %equation to convert 2D lift curve slope to 3D lift curve slope
