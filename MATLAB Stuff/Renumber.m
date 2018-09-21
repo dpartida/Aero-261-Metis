cf40ae9d96c6f787e25555ee2f2725605150ca29
dpartida <31483393+dpartida@users.noreply.github.com>
2018-09-21 17:27:29 -0500

    Changes to MATLAB Re

--- MATLAB Stuff/Renumber.m
+++ MATLAB Stuff/Renumber.m
@@ -6,10 +6,14 @@ clear,clc
 %1 ft=0.3048 m | 1 kg=0.0685218 slug | 1 N=0.224809 lb |
 %1 (lb*s)/ft^2=47.88(N*s)/m^2
 
-p=input('What is the air density at the given altitude in kg/m^3?: '); %units kg/m^3 
-v=input('What is the air speed at the given altitude in m/s?: '); %units m/s
-c=input('What is the mean chord length in m?: ');
-u=1.7907*(10^-5); %units (N*s)/m^2
+%print statement warning user to keep units in check
+fprintf('**Make sure units match**\n-------------------------\n')
 
-Re=(p*v*c)/u; %equation to calculate Reynold's Number
-fprintf('Re is %f',Re)
+p=input('Air density(p) at the given altitude (kg/m^3)?: '); %air density -units kg/m^3 
+v=input('Air speed(v) (m/s)?: '); %relative airspeed -units m/s
+S=input('Planform area(S) (m^2)?: '); %wing planform area -units m^2
+b=input('Wingspan(b) (m): '); %wingspan -units m
+u=1.7907*(10^-5); %air viscosity -units (N*s)/m^2
+
+Re=(p*v*(S/b))/u; %equation to calculate Reynold's Number
+fprintf('Re = %f\n',Re)
